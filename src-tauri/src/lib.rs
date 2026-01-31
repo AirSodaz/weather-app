08e2afd65e3f5b745bad7372fb1abef53ddb32b9
AirSodaz <asodaz@outlook.com>
2026-01-31 16:41:33 +0800

    fix: Android build issue

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,9 +4,14 @@
 /// This function is also used as the mobile entry point.
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_window_state::Builder::default().build())
+    let mut builder = tauri::Builder::default().plugin(tauri_plugin_shell::init());
+
+    #[cfg(not(mobile))]
+    {
+        builder = builder.plugin(tauri_plugin_window_state::Builder::default().build());
+    }
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
